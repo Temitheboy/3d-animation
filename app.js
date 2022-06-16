b5b486813afcf428cbec4322125022299eea1817
Temitope Agbor <67988734+Temitheboy@users.noreply.github.com>
2022-06-16 23:14:32 +0100

    imports and updates

---
+++ app.js
@@ -0,0 +1,8 @@
+const iPhone = document.querySelector(".iPhone");
+const container = document.querySelector(".container");
+
+// mouse move over
+
+container.addEventListener('mousemove', (e) => {
+    let xAxis
+})
